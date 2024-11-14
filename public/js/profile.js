1a88e19434e5f2d315f246424489bbebf27ef0e3
EfrosEM <eduardoespinosamerono@gmail.com>
2024-11-14 01:17:26 +0100

    Estadisticas de usuario actualizadas

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,19 +6,66 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
         const usuario = await response.json();
 
+        // Calcular porcentajes de partidas ganadas y perdidas
+        const totalGames = usuario.games;
+        const wins = usuario.wins;
+        const losses = totalGames - wins;
+        const winPercentage = (wins / totalGames) * 100;
+        const lossPercentage = (losses / totalGames) * 100;
+
+        // Calcular porcentajes de fallos y aciertos
+        const totalPreguntas = usuario.preguntas;
+        const aciertos = usuario.aciertos;
+        const fallos = totalPreguntas - aciertos;
+        const aciertoPercentage = (aciertos / totalPreguntas) * 100;
+        const falloPercentage = (fallos / totalPreguntas) * 100;
+
         // Mostrar los datos en el HTML
         document.getElementById('username').textContent = usuario.user;
         document.getElementById('email').textContent = usuario.email;
         document.getElementById('points').textContent = usuario.points;
         document.getElementById('alta').textContent = usuario.alta;
-        document.getElementById('wins').textContent = usuario.wins;
         document.getElementById('games').textContent = usuario.games;
-        document.getElementById('aciertos').textContent = usuario.aciertos;
         document.getElementById('preguntas').textContent = usuario.preguntas;
-        document.getElementById('time').textContent = usuario.time;
+        document.getElementById('time').textContent = segundosToHHMMSS(usuario.time);
 
+        if (totalGames > 0) {
+            document.getElementById('wins').textContent = Math.round(winPercentage);
+            // Actualizar las barras de progreso en función de las partidas ganadas y perdidas
+            document.querySelector('.progress .progress-bar[aria-label="Partidas ganadas"]').style.width = `${winPercentage}%`;
+            document.querySelector('.progress .progress-bar[aria-label="Partidas ganadas"]').textContent = wins;
+            document.querySelector('.progress .progress-bar[aria-label="Partidas perdidas"]').style.width = `${lossPercentage}%`;
+            document.querySelector('.progress .progress-bar[aria-label="Partidas perdidas"]').textContent = losses;
+        }
+        else {
+            document.getElementById('wins').textContent = 0;
+        }
+        if (totalPreguntas > 0) {
+            document.getElementById('aciertos').textContent = Math.round(aciertoPercentage);
+            // Actualizar las barras de progreso en función de los fallos y aciertos
+            document.querySelector('.progress .progress-bar[aria-label="Aciertos"]').style.width = `${aciertoPercentage}%`;
+            document.querySelector('.progress .progress-bar[aria-label="Aciertos"]').textContent = aciertos;
+            document.querySelector('.progress .progress-bar[aria-label="Fallos"]').style.width = `${falloPercentage}%`;
+            document.querySelector('.progress .progress-bar[aria-label="Fallos"]').textContent = fallos;
+        }
+        else {
+            document.getElementById('aciertos').textContent = 0;
+        }
+        
     } catch (error) {
         console.error('Error al obtener los datos del perfil:', error);
         window.location.href = '/login.html';  // Redirigir si no está autenticado
     }
+
 });
+
+function segundosToHHMMSS(segundos) { 
+    let horas = Math.trunc(segundos / 3600);
+    segundos %= 3600;
+    let minutos = Math.trunc(segundos / 60);
+    segundos %= 60;
+
+    let cadena = horas + 'h ' + minutos + 'min ' + segundos + 's';
+
+    return cadena;
+}
