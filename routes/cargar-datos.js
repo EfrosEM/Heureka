413551e336b6e56c43a74fc632b89b466a38ec06
EfrosEM <eduardoespinosamerono@gmail.com>
2024-11-16 13:21:18 +0100

    Ruta para cargar usuarios de prueba en la BD

--- routes/cargar-datos.js
+++ routes/cargar-datos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Tarjeta = require('../models/Tarjeta');
 const Heuristica = require('../models/Heuristica');
+const Usuario = require('../models/Usuario');
 
 const tarjetas = require('../tarjetas.json'); // Archivo con las tarjetas
 const heuristicas = require('../heuristicas.json'); // Archivo con las heurísticas
@@ -39,6 +40,33 @@ router.post('/cargar-datos', async (req, res) => {
     } catch (error) {
         console.error('Error al cargar los datos:', error);
         return res.status(500).json({ success: false, msg: 'Error al cargar los datos', error: error.message });
+    }
+});
+
+// Ruta para subir usuarios de prueba a la base de datos
+router.post('/cargar-usuarios', async (req, res) => {
+    try {
+        for (i = 0; i < 20; i++) {
+            const nuevoUsuario = new Usuario({
+                user: `user${i}`,
+                email: `user${i}@um.es`,
+                password: `user${i}`,
+                points: 0,
+                alta: new Date(Date.now()),
+                games: 0,
+                wins: 0,
+                preguntas: 0,
+                aciertos: 0,
+                time: 0
+            });
+            await nuevoUsuario.save();
+            console.log('Usuario guardado:', nuevoUsuario);
+        }
+
+        return res.status(200).json({ success: true, msg: 'Usuarios cargados correctamente' });
+    } catch (error) {
+        console.error('Error al cargar los usuarios:', error);
+        return res.status(500).json({ success: false, msg: 'Error al cargar los usuarios', error: error.message });
     }
 });
 
